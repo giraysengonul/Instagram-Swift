fd9778f48e6fb6b382d9c602cae78888b9d4b30c
hakkicansengonul <coder.9734@gmail.com>
2022-08-17 21:12:09 +0300

    reusability has been adjusted

---
+++ Instagram-Swift/Utils/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Instagram-Swift
+//
+//  Created by hakkı can şengönül on 17.08.2022.
+//
+
+import Foundation
---
+++ Instagram-Swift/View/CustomTextField.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTextField.swift
+//  Instagram-Swift
+//
+//  Created by hakkı can şengönül on 17.08.2022.
+//
+
+import Foundation
